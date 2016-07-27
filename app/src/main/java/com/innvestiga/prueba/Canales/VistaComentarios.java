ec6fa0169225c3f122e808d232db1790bb7cd4bd
Ariel Torres <arielpt16@gmail.com>
2016-07-26 21:52:59 -0600

    diseños extras
    diseños de fernando

---
+++ app/src/main/java/com/innvestiga/prueba/Canales/VistaComentarios.java
@@ -0,0 +1,30 @@
+package com.innvestiga.prueba.Canales;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.innvestiga.prueba.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class VistaComentarios extends Fragment {
+
+
+    public VistaComentarios() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_vista_comentarios, container, false);
+    }
+
+}
